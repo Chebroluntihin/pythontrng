dd8c393ca8a4f3713769246e53757922be983b06
Ch.Nithin <nithinchebrolu20@gmail.com>
2021-09-28 13:05:52 +0530

    files created

---
+++ Q1.py
@@ -0,0 +1,12 @@
+x=10
+y=50
+z=30
+if(x>y) and (x>z):
+    big=x
+    print(big)
+elif(y>x) and (y>z):
+    big1=y
+    print(big1)
+else:
+    big3=z
+    print(big3)
---
+++ ifcond.py
@@ -0,0 +1,10 @@
+x=(int(input("Enter an integer")))
+y=(int(input("Enter an integer")))
+if(x>y):
+    sum=x+y
+    print(sum)
+elif(x<y):
+    sub=x-y
+    print(sub)
+else:
+    print("No")
